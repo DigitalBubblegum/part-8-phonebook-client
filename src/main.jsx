37ca4865c2cbc999dd763c738a6f7135ff2c58f8
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-01 11:22:40 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+query{
+  allPersons {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+client.query({query}).then((response) => {
+  console.log('i have sent the query')
+  console.log(response.data)
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
