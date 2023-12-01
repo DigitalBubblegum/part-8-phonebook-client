0a51ff5d8939cc2255f049cf63cb7fa6ebcc0cab
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-01 11:30:25 +0530

    -useQuery to fetch data from server

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import { ApolloClient,ApolloProvider, InMemoryCache, gql } from '@apollo/client'
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
