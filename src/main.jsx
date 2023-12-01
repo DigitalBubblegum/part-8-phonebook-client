887430e775a048f0b3ad4cdcd40263833ed765e3
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-01 11:25:28 +0530

    -client access to all compo with apolloprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient,ApolloProvider, InMemoryCache, gql } from '@apollo/client'
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
@@ -27,7 +26,7 @@ client.query({query}).then((response) => {
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
