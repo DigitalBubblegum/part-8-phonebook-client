1c5590c1be2fcf58d0c11bf15ca03a76e87c753c
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-06 12:44:07 +0530

    -adding a token to the header

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,22 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
-import { ApolloClient,ApolloProvider, InMemoryCache, gql } from '@apollo/client'
-const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
-})
-
-const query = gql`
-query{
-  allPersons {
-    name,
-    phone,
-    address {
-      street,
-      city
+import { ApolloClient,ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext} from '@apollo/client/link/context'
+const authLink = setContext((_,{headers})=>{
+  const token = localStorage.getItem('phonenumbers-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
     }
-    id
   }
-}
-`
-client.query({query}).then((response) => {
-  console.log('i have sent the query')
-  console.log(response.data)
+})
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
